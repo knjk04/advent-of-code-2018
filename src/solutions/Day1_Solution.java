0f5d58b91b0e1fa61e7e1b0975c2ea4ebe58fa90
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-12-01 14:40:51 +0000

    add day 1, part 2

--- src/solutions/Day1_Solution.java
+++ src/solutions/Day1_Solution.java
@@ -1,12 +1,18 @@
 package solutions;
 
 import utils.Util;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class Day1_Solution {
+    // HashSet instead of ArrayList because it can search (contains()) in constant time
+    private HashSet<Integer> frequencies = new HashSet<>();
+    private int resultingFrequency = 0;
+
     public static void main(String[] args) {
         List<String> input = Util.readInput("/day01.txt");
-        new Day1_Solution().runPartOne(input);
+        new Day1_Solution().runPartTwo(input);
     }
 
     private void runPartOne(List<String> input) {
@@ -17,5 +23,36 @@ public class Day1_Solution {
             resultingFrequency = (sign == '+') ? (resultingFrequency + frequency) : (resultingFrequency - frequency);
         }
         System.out.println(resultingFrequency);
+    }
+
+    private void runPartTwo(List<String> input) {
+        frequencies.add(resultingFrequency);
+        while (true) {
+            if (findDuplicateFrequency(input)) {
+                break;
+            }
+        }
+        System.out.println("duplicate frequency = " + resultingFrequency);
+    }
+
+    private boolean findDuplicateFrequency(List<String> input) {
+        for (String line : input) {
+            if (line.isEmpty()) {
+                break;
+            }
+            System.out.println("line = " + line);
+            char sign = line.charAt(0);
+            int frequency = Integer.parseInt(line.substring(1));
+            resultingFrequency = (sign == '+') ? (resultingFrequency + frequency) : (resultingFrequency - frequency);
+
+            if (frequencies.contains(resultingFrequency)) {
+                System.out.println("found duplicate");
+                return true;
+            } else {
+                frequencies.add(resultingFrequency);
+                System.out.println("resultingFrequency = " + resultingFrequency);
+            }
+        }
+        return false;
     }
 }
